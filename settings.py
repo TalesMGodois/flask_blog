9c276aa71f7be1fb307fc746ac2c8a20a6fd9a02
Tales Marinho Godois <tales.godois@gmail.com>
2018-03-26 10:33:02 -0300

    Com essa base é possível comecar a trabalhar

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
-# import os
+import os
 
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
+
+DB_USERNAME = 'bblog'
+DB_PASSWORD = 'chacal'
+BLOG_DATABASE_NAME = 'blog'
+DB_HOST = os.getenv('IP', '0.0.0.0')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s/%s' %(DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+SQLALCHEMY_TRACK_MODIFICATIONS = True
